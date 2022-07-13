64382f6887d004f8dab351acf5893b8714cdfa76
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-12 20:55:43 -0400

    Update comentario.js

---
+++ Fundamentos/Inicio/comentario.js
@@ -0,0 +1,10 @@
+// console.log é usado para exibir algo na tela do computador!
+console.log("Passo #01");
+console.log("Passo #02");
+console.log("Passo #03");
+
+/*
+Este é um 
+comentário para
+múltiplas linhas
+*/
