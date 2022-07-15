066beadb22b58612c9a9faeb4ae8c105f43b1494
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-14 21:57:31 -0300

    Create dados_3.js

---
+++ Fundamentos/Inicio/dados_3.js
@@ -0,0 +1,10 @@
+let idade = 31;
+let salario = 4578.32;
+let estaChovendo = true; // ou false
+let texto = "teste";
+
+console.log(typeof idade);
+console.log(typeof 31);
+console.log(typeof salario); // Javascript não faz distinção entre tipos numéricos (int e float)
+console.log(typeof estaChovendo);
+console.log(typeof texto);
