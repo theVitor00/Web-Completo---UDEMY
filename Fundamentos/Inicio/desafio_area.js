729fcb9858ba638a567320b08e03cf6bb468beff
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-14 22:14:59 -0300

    Create desafio_area.js

---
+++ Fundamentos/Inicio/desafio_area.js
@@ -0,0 +1,11 @@
+/*
+Calcule a área de uma circunferência. Considere a seguinte fórmula:
+PI * raio * raio
+*/
+
+const PI = 3.141592;
+let raio = 10;
+let area = PI * (raio * raio);
+
+console.log("A área da circunferência de raio ", raio, " é de ", area);
+console.log(Math.PI);
