db3ac00a9ed769af6ac705f19a1cf2ff78b6f691
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-15 21:10:22 -0300

    Update desafio_troca.js

---
+++ Fundamentos/Inicio/desafio_troca.js
@@ -0,0 +1,15 @@
+/*
+crie duas variáveis e atribua valores a elas. depois troque o valor de uma pelo da outra
+*/
+
+a = 7;
+b = 94;
+
+console.log("A = ", a);
+console.log("B = ", b);
+c = a;
+a = b;
+b = c;
+
+console.log("O valor de A depois da troca é: ", a);
+console.log("O valor de B depois da troca é: ", b);
