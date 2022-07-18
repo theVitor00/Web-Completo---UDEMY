a26d023a46aff02415b8472fa9929a58cb00ee2a
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-17 21:51:44 -0300

    Update organizacao.js

---
+++ EXERCICIOS-JS/FUNDAMENTOS/organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código"); // isso é uma sentença
+
+{
+    {
+    // Blocos tem a função de agrupar sentenças de códigos, e podem ser delimitados por chaves
+    console.log("Olá");
+    console.log("Mundo!");
+    }
+}
