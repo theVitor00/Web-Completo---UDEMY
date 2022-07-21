f1caa56d80aa8a2bf118591f3dd198e802ed105d
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-21 20:52:44 -0300

    Update numerosAlgunsCuidados.js

--- Fundamentos/Inicio/numerosAlgunsCuidados.js
+++ Fundamentos/Inicio/numerosAlgunsCuidados.js
@@ -1,6 +1,8 @@
 console.log(7/0);               // --- Divisão por Zero retorna "Infinity"
 console.log(typeof Infinity);
 console.log("10" / 2);          // --- String numéricas são convertidas automaticamente durante operações
+console.log("3" + 2);           // --- A string tem preferência na operação, e o compilador concatena
+console.log("3" - 2);           // --- Subtração não faz sentido na operação com strings, então é realizada a operação numérica    
 console.log("Show!" * 2);       // --- Retorna "Not a Number", ou simplesmente NaN
 console.log(typeof NaN);
 console.log(0.1 + 0.7);         // --- Soma bugada. Pesquisar a respeito
