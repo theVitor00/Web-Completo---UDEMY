4b530fee0da591df3b4c8defdfd81d9d0d16a1ac
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-21 16:09:07 -0300

    Aula sobre tipos numéricos

--- Fundamentos/tipagemFraca.js
+++ Fundamentos/tipagemFraca.js
@@ -1,7 +1,11 @@
+//Javascrip é uma linguagem de Tipagem Fraca
+
 let qualquer = "Legal";
 console.log(qualquer);
-console.log(typeof qualquer);
+console.log(typeof qualquer); //typeof mostra o tipo de dado apresentado
 
 qualquer = 3.1516;
 console.log(qualquer);
 console.log(typeof qualquer);
+
+//Evitar nomes genéricos ou siglas para variáveis, métodos ou funções
