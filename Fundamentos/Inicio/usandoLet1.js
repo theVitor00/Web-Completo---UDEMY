9a72a61d6a864a2f41adc41f5c9aebbef5eb8991
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-22 11:43:31 -0300

    Update usandoLet1.js

--- Fundamentos/Inicio/usandoLet1.js
+++ Fundamentos/Inicio/usandoLet1.js
@@ -1,5 +1,5 @@
 /*
-Variáveis criadas com 'let' possuem escopo local em blocos de código, e não são visíveis fora das chaves
+Variáveis criadas com 'let' possuem escopo local em blocos de código, função e escopo global, e não são visíveis fora das chaves
 */
 var numero = 1
 {
