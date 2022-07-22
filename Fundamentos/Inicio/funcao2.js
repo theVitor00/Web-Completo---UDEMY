13b7b4896a1f3bdd6c48b5a840803d8fc0f7f1c7
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-22 11:05:43 -0300

    Create funcao2.js

---
+++ Fundamentos/Inicio/funcao2.js
@@ -0,0 +1,17 @@
+// Armazenando uma função em uma variável ou constane
+const imprimirSoma = function(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Armazenando uma função Arrow em uma variável
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// Função Arrow de retorno implícito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
