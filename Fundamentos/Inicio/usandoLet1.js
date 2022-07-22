0ed00814ab81bf3d05485700efcdda2d89c721e2
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-07-22 11:42:06 -0300

    Create usandoLet1.js

---
+++ Fundamentos/Inicio/usandoLet1.js
@@ -0,0 +1,9 @@
+/*
+Variáveis criadas com 'let' possuem escopo local em blocos de código, e não são visíveis fora das chaves
+*/
+var numero = 1
+{
+    let numero = 2
+    console.log('dentro = ', numero)
+}
+console.log('fora = ', numero)
