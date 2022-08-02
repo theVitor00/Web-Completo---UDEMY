1bb7ddc7be525e77aa18dd92b04146b0c781755a
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-08-01 21:06:26 -0300

    Update ternario.js

--- Fundamentos/Inicio/ternario.js
+++ Fundamentos/Inicio/ternario.js
@@ -5,7 +5,7 @@ A função 'resultado' vai receber o parâmetro 'nota' e analisá-lo
 Se nota for maior ou igual a 7 (nota >= 7), irá retornar 'Aprovado';
 Caso não seja, irá retornar 'Reprovado'.
 
-O Operador Ternário é uma versão reduzida da estrutura if/else
+O Operador Ternário é uma versão reduzida da estrutura if/else.
 */
 const resultado = nota => nota >= 7 ? 'Aprovado' : 'Reprovado'
 
