5f686bc49f29a8b9f20e1d7e32472e263f71a4e8
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-08-14 16:13:03 -0300

    Update paramsVariaveis.js

--- Fundamentos/funcao/paramsVariaveis.js
+++ Fundamentos/funcao/paramsVariaveis.js
@@ -1,7 +1,7 @@
 function soma() {
     let soma = 0
     for(i in arguments) {
-        soma += arguments[i]
+        soma += arguments[i] //'arguments' é uma array interna que aceita multiplos argumentos numa funcao
     }
     return soma
 }
