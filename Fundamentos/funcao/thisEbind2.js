9b85b53320414319b4fced467367d1f84d63bbbc
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-08-14 22:05:13 -0300

    Update thisEbind2.js

--- Fundamentos/funcao/thisEbind2.js
+++ Fundamentos/funcao/thisEbind2.js
@@ -1,7 +1,7 @@
 function Pessoa () {
     this.idade = 0
 
-    const self = this
+    const self = this       // Ao associar o self a uma constante, ele já não varia de acordo com quem chama
     setInterval(function () {
         self.idade++
         console.log(self.idade)
