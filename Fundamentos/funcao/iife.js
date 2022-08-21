49e7696fc6202166570dbfa845e66b63dd6c9a8c
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-08-21 16:12:03 -0300

    Create iife.js

---
+++ Fundamentos/funcao/iife.js
@@ -0,0 +1,7 @@
+// IIFE - Imeditately Invoked Function Expression
+// Função anônima criada entre parentesis e chamada com parentesis vazios ao fim da declaração
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Fora do escopo mais abrangente!')
+})()
