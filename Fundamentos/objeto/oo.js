01fd5688b1147a54e4abbf618bcba5c0a65600b6
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-08-25 22:50:52 -0300

    Create oo.js

---
+++ Fundamentos/objeto/oo.js
@@ -0,0 +1,26 @@
+// CÓDIGO NÃO EXECUTÁVEL //
+
+// Procedural
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+objeto.processamento()
+
+/*
+PRINCÍPIOS DA ORIENTAÇÃO A OBJETOS
+
+--> ABSTRAÇÃO - Traduzir o mundo real para um modelo computacional aplicável.
+--> ENCAPSULAMENTO - Ocultar dentro do objeto as partes complexas do código, deixando disponíveis apenas as 
+partes que são necessárias para a operação, como uma interface.
+--> HERANÇA - Recebe atributos e comportamentos de um Objeto-pai. 
+--> POLIMORFISMO - Possui múltiplas formas.
+
+*/
