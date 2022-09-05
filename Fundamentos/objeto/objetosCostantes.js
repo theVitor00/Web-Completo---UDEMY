4a36e969edfa767680c685b3948e360f69fae4aa
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-09-04 21:56:42 -0300

    Update objetosCostantes.js

--- Fundamentos/objeto/objetosCostantes.js
+++ Fundamentos/objeto/objetosCostantes.js
@@ -3,3 +3,17 @@ const pessoa = {nome: 'João'}
 pessoa.nome = 'Pedro'
 
 console.log(pessoa)
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+console.log(pessoa.nome)
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({nome: 'João'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
