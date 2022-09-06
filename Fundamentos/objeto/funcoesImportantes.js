84b1a84aaf4ff03ad8f939ff26d43c137540e736
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-09-05 22:27:30 -0300

    Create funcoesImportantes.js

---
+++ Fundamentos/objeto/funcoesImportantes.js
@@ -0,0 +1,23 @@
+const pessoa = {
+    nome: 'Rebecca',
+    idade: 2,
+    peso: 13
+}
+
+console.log(Object.keys(pessoa))
+console.log(Object.values(pessoa))
+console.log(Object.entries(pessoa))
+
+Object.entries(pessoa).forEach(e => {
+    console.log(`${e[0]}: ${e[1]}`)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+    enumerable: true,
+    writable: false,
+    value: '01/01/2019'
+})
+
+pessoa.dataNascimento = '01/01/2017'
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
