6381f56dd60ea614041b36b6f1fadbfab8356af7
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-09-25 16:27:23 -0300

    Update heranca5.js

--- Fundamentos/objeto/heranca5.js
+++ Fundamentos/objeto/heranca5.js
@@ -1,3 +1,16 @@
 console.log(typeof String)
 console.log(typeof Array)
 console.log(typeof Object)
+
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('')
+}
+
+console.log('Escola Cod3r'.reverse())
+
+Array.prototype.first = function () {
+    return this[0]
+}
+
+console.log([2, 4, 6, 8, 10].first())
+console.log(['Alpha', 'Beta', 'Gama', 'Delta'].first())
