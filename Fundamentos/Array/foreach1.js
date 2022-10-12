964f2b6f44ab784b8d7b7068e9e8705d72b9fa2c
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-12 16:55:20 -0300

    Update foreach1.js

--- Fundamentos/Array/foreach1.js
+++ Fundamentos/Array/foreach1.js
@@ -1,5 +1,6 @@
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
+//Função callback q passamos para um forEach só recebe 3 parametros: nome, indice e aray
 aprovados.forEach(function(nome, indice, array) {
     console.log(`${indice + 1}) ${nome}`)
     console.log(array)
