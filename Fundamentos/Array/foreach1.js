aa605ea400f6e5f6132f48c52f89b3fce3055e5f
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-12 16:54:00 -0300

    Update foreach1.js

--- Fundamentos/Array/foreach1.js
+++ Fundamentos/Array/foreach1.js
@@ -1,7 +1,8 @@
 const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
 
-aprovados.forEach(function(nome, indice) {
+aprovados.forEach(function(nome, indice, array) {
     console.log(`${indice + 1}) ${nome}`)
+    console.log(array)
 })
 
 aprovados.forEach(nome => console.log(nome))
