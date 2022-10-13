5b01f06be9f77735a3a70da083c26e4d92487394
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-12 21:52:29 -0300

    Create map2.js

---
+++ Fundamentos/Array/map2.js
@@ -0,0 +1,8 @@
+const carrinho = [
+    '{ "nome:", "Borracha", "preço:", 3.45 }',
+    '{ "nome:", "Caderno", "preco:", 13.90 }',
+    '{ "nome:", "Kit de Lapis", "preco:", 41.22 }',
+    '{ "nome:", "Caneta", "preco:", 7.50}'
+]
+
+// Retornar uma array apenas com os preços
