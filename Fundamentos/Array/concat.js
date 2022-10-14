206455f870289f70584064848964d6978225d371
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-14 18:34:36 -0300

    Create concat.js

---
+++ Fundamentos/Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Waleska', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos, 'Jucicleidson')
+
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
