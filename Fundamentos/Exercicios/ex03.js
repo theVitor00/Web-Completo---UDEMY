97e41b34175da3d6e2cf15d45aa2756031c496bd
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-14 21:04:48 -0300

    Create ex03.js

---
+++ Fundamentos/Exercicios/ex03.js
@@ -0,0 +1,9 @@
+/* 
+Crie uma função que recebe dois parâmetris, base e expoente, e retorne a base elevada ao expoente
+*/
+
+const potencia = function(base, expoente) {
+    return Math.pow(base, expoente)
+}
+
+console.log(potencia(4, 2))
