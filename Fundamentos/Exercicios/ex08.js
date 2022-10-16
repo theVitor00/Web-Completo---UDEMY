bdb01c82d060c74dd672388d81eb8640f4fc19c8
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-16 10:20:46 -0300

    Update ex08.js

--- Fundamentos/Exercicios/ex08.js
+++ Fundamentos/Exercicios/ex08.js
@@ -6,7 +6,19 @@ Dada uma lista String = 'Pontuação1', 'Pontuação2', 'Pontuação3', etc. esc
 comparar os valores e irá retornar um vetor com o número de vezes que ele bateu seu recorde de maior número de
 pontos e quando fez seu pior jogo. (número do pior jogo).
 */
+const resultados = [5, 10, 12, 3, 46, 16, 50, 30]
 
-const analisadorTatico = function (lista) {
-    
+const retornarAnalise = function (resultados) {
+    for (let i = 0; i < resultados.length; i++){
+        let controle = 0
+        let recordes = 0
+
+        if (resultados[i + 1] > resultados[i] && resultados[i + 1] > controle) {
+            this.controle = resultados[i + 1]
+            this.recordes++
+        }
+    }
+    return this.controle, this.recordes
 }
+
+retornarAnalise(resultados, controle, recordes)
