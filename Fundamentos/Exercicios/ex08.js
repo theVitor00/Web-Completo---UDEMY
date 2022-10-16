5f3ed31a8843a4bb02ee235926165aaf341f3097
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-16 09:57:27 -0300

    Create ex08.js

---
+++ Fundamentos/Exercicios/ex08.js
@@ -0,0 +1,12 @@
+/* 
+Pedro joga N jogos de basquete por temporda. Para saber como está progredindo, ele mantém registro de todas
+as pontuações feitas por jogo. Após cada jogo, ele anota no novo valor e confere se o mesmo é maior ou menor que 
+seu melhor e que seu pior desempenho.
+Dada uma lista String = 'Pontuação1', 'Pontuação2', 'Pontuação3', etc. escreva uma função que ao recebê-la, irá
+comparar os valores e irá retornar um vetor com o número de vezes que ele bateu seu recorde de maior número de
+pontos e quando fez seu pior jogo. (número do pior jogo).
+*/
+
+const analisadorTatico = function (lista) {
+    
+}
