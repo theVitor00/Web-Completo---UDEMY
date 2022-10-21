84932f9cc09110efa9bde4ce61671be2474bca6a
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 22:42:04 -0300

    Update ex11.js
    Exercício resolvido e funcional

--- Fundamentos/Exercicios/ex11.js
+++ Fundamentos/Exercicios/ex11.js
@@ -8,3 +8,21 @@ As regras para o cálculo dos anos bissextos são as seguintes:
 Partindo daí, elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a 
 mensagem e retornando true ou false
 */
+
+const isBissexto = function (ano) {
+    let resposta
+
+    if (ano % 400 == 0) {
+        resposta = true
+    } else if (ano % 100 == 0) {
+        resposta = false
+    } else if (ano % 4 == 0) {
+        resposta = true
+    } else {
+        resposta = false
+    }
+
+    return `O Ano de ${ano} é Bissexto: ${resposta}`
+}
+
+console.log(isBissexto(1401))
