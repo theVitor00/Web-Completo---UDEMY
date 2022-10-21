03aca645df71de514c7337debd2217b4d52e873a
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 22:27:42 -0300

    Create ex15.js

---
+++ Fundamentos/Exercicios/ex15.js
@@ -0,0 +1,6 @@
+/* 
+Um homem decidiu ir a uma revenda comprar um carro. Ele deseja comprar um carro Hatch, e a revenda possui, além
+de carros Hatch, Sedan, Caminhonetes e Motocicletas. Utilizado uma estrutura switch, caso o comprador queira o 
+Hatch, retorne "Compra efetuada com sucesso". Nas outras, retorne "Tem certeza que não prefere esse modelo?". 
+Caso seja especificado um modelo que não está disponível, retorne "Não trabalhamos com este tipo de automóvel."
+*/
