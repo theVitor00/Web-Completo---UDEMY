79110d57160338e6ed2ebfd1d233afed4905e637
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 23:04:12 -0300

    Update ex14.js
    Exercício resolvido e funcionando

--- Fundamentos/Exercicios/ex14.js
+++ Fundamentos/Exercicios/ex14.js
@@ -4,3 +4,35 @@ Caso maçã, retorne no console "Não vendemos essa fruta aqui". Caso kiwi, reto
 Caso melancia, retorne "Aqui está! São 3 reais o quilo". Teste com essas três opções. Crie também um default, que
 retornará uma mensagem de erro no console.
 */
+
+const analisarFruta = function (fruta) {
+    let resposta
+    let retorno
+    if(fruta == 'Maçã' || fruta == 'maçã' || fruta == 'maca') {
+        resposta = 1
+    } else if (fruta == 'Kiwi' || fruta == 'kiwi') {
+        resposta = 2
+    } else if (fruta == 'Melancia' || fruta == 'melancia') {
+        resposta = 3
+    } else {
+        resposta = null
+    }
+
+    switch (resposta) {
+        case 1:
+            retorno = 'Não vendemos essa fruta aqui.'
+            break
+        case 2:
+            retorno = 'Estamos com escassez de Kiwi.'
+            break
+        case 3:
+            retorno = 'Aqui está! São 3 reais o quilo!'
+            break
+        default:
+            retorno = 'Produto Inválido!'
+
+    }
+    return retorno
+}
+
+console.log(analisarFruta('Abacate'))
