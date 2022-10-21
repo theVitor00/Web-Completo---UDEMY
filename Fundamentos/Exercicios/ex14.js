60bb323e9e831da9b1b51a478daaa96132da55df
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 22:24:05 -0300

    Update ex14.js

---
+++ Fundamentos/Exercicios/ex14.js
@@ -0,0 +1,6 @@
+/* 
+Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três caso:
+Caso maçã, retorne no console "Não vendemos essa fruta aqui". Caso kiwi, retorne "Estamos com escassez de kiwis".
+Caso melancia, retorne "Aqui está! São 3 reais o quilo". Teste com essas três opções. Crie também um default, que
+retornará uma mensagem de erro no console.
+*/
