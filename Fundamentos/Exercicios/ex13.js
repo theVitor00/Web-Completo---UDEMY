6a361adfc2e5ea4e266718fa7c3fcbfc2589d0b0
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 22:21:48 -0300

    novos exercícios

---
+++ Fundamentos/Exercicios/ex13.js
@@ -0,0 +1,4 @@
+/* 
+Crie um programa que exibe se um dia é útil, fim de semana ou dia inválido dado o nome referente ao dia. Considere
+que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura switch
+*/
