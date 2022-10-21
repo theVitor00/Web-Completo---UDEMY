b88012f47552b1f1a6e21eeb387147ba1052025f
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 23:12:18 -0300

    Update ex15.js
    Exercício resolvido e funcional

--- Fundamentos/Exercicios/ex15.js
+++ Fundamentos/Exercicios/ex15.js
@@ -4,3 +4,30 @@ de carros Hatch, Sedan, Caminhonetes e Motocicletas. Utilizado uma estrutura swi
 Hatch, retorne "Compra efetuada com sucesso". Nas outras, retorne "Tem certeza que não prefere esse modelo?". 
 Caso seja especificado um modelo que não está disponível, retorne "Não trabalhamos com este tipo de automóvel."
 */
+
+const comprarCarro = function (modelo) {
+    let controle
+    let resposta
+    if (modelo == 'Hatch') {
+        controle = 1
+    } else if (modelo == 'Sedan' || modelo == 'Caminhonete' || modelo == 'Motocicleta') {
+        controle = 2
+    } else {
+        controle = null
+    }
+
+    switch (controle) {
+        case 1:
+            resposta = 'Compra efetuada com sucesso!'
+            break
+        case 2:
+            resposta = 'Tem certeza que não prefere este modelo?'
+            break
+        default:
+            resposta = 'Não trabalhamos com este modelo de automóvel.'
+            break
+    }
+    return resposta
+}
+
+console.log(comprarCarro('Hatch'))
