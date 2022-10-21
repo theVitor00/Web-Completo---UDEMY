63f334f92ff1c4a8da6b9bfd471777b551b8079f
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-20 22:50:35 -0300

    Update ex12.js
    Exercício resolvido e funcional

--- Fundamentos/Exercicios/ex12.js
+++ Fundamentos/Exercicios/ex12.js
@@ -1,3 +1,15 @@
 /* 
 Faça um algoritmo que calcule o fatorial de um número.
 */
+
+const calculaFatorial = function (num) {
+    let fatorial = 1
+    let controle = num
+    for (let i = num; i >= 1; i--) {
+        fatorial = fatorial * controle
+        controle -= 1
+    }
+    return `${num}! = ${fatorial}`
+}
+
+console.log(calculaFatorial(5))
