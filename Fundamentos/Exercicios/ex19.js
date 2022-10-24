04608460d68906e7148fc73979dd84a7289021ff
theVitor00 <68963979+theVitor00@users.noreply.github.com>
2022-10-24 11:38:50 -0300

    Create ex19.js

---
+++ Fundamentos/Exercicios/ex19.js
@@ -0,0 +1,14 @@
+/* O cardápio de uma lanchonete é o seguinte:
+
+CÓDIGO      DESCRIÇÃO DO PRODUTO             PREÇO
+100           Cachorro Quente                R$ 3,00
+200           Hambúrguer Simples             R$ 4,00
+300             Cheeseburguer                R$ 5,50
+400                 Bauru                    R$ 7,50
+500             Refrigerante                 R$ 3,50
+600                  Suco                    R$ 2,80
+
+Implemente uma função que receba como parâmetros o código do item pedido, a quantidade e calcule o valor
+a ser pago por aquele lanche. Considere que a cada execução somente será calculado um item. Use o
+comando switch. Crie um caso default para produto não existente.
+*/
